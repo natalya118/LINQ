008f120735f60184878b31b74a878c04890f5db7
natalya118 <nutblog88@gmail.com>
2016-07-06 08:34:04 +0300

    task3 solution added

---
+++ Dranchuk_week3/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+
+    class FootballTeam : IComparable<FootballTeam>, IParsable
+    {
+        public string Name { get; private set; }
+        public DateTime LastGame { get; private set; }
+        public string Rival;
+        public static string regex = @"(w+)\shas\swon\sa\smatch\son\s(\d{4}/\d\d/\d\d)\swith(w+)";
+        public FootballTeam(string name, DateTime game, string rival)
+        {
+            Name = name;
+            LastGame = game;
+            Rival = rival;
+        }
+
+        //get obj from xml
+        public FootballTeam GetFromXML(string filename)
+        {
+            
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreWhitespace = true;
+            using (XmlReader r = XmlReader.Create(filename, settings))
+            {
+                r.MoveToContent(); 
+                r.ReadStartElement("team");
+                string TeamName = r.ReadElementContentAsString("team-name", "");
+                DateTime LastG = DateTime.Parse(r.ReadElementContentAsString("last-game", ""));
+                string Rival = r.ReadElementContentAsString("rival", "");
+                r.MoveToContent(); 
+                r.ReadEndElement();
+                return new FootballTeam(TeamName, LastG, Rival);
+            }
+
+            
+        }
+        public string GetRegex()
+        {
+            return regex;
+        }
+        public FootballTeam(string s)
+        {
+            var match = Regex.Matches(s, regex);
+            Name = (string)match[1].Value;
+            LastGame = DateTime.Parse(match[2].Value);
+
+        }
+
+        public int CompareTo(FootballTeam ft)
+        {
+            return (this.Name).CompareTo(ft.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(Name, LastGame).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, last game: {LastGame}";
+        }
+
+        public List<IParsable> Parse(string text)
+        {
+
+            var matches = Regex.Matches(text, regex);
+
+            List<IParsable> res = new List<IParsable>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                res.Add(new FootballTeam(matches[i].Value));
+
+            }
+
+            return res;
+
+        }
+
+        public List<FootballTeam> RemoveDuplicates(List<FootballTeam> l)
+        {
+            List<FootballTeam> res = l.Distinct(new FTComparer()).ToList();
+            return res;
+        }
+
+        //add obj info to xml file
+        public void SaveAsXML()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            using (XmlWriter writer = XmlWriter.Create("D:\FootballTeams\ft.xml", settings))
+            {
+                writer.WriteStartElement("team");
+                writer.WriteStartElement("team-name", Name);
+
+                writer.WriteElementString("last-game", LastGame.ToString());
+                writer.WriteElementString("rival", Rival);
+                writer.WriteEndElement();
+            }
+        }
+
+    }
+    
+    //needed to remove duplicates
+    class FTComparer : IEqualityComparer<FootballTeam>
+    {
+
+        public bool Equals(FootballTeam x, FootballTeam y)
+        {
+            return x.Name.Equals(y.Name);
+        }
+
+        public int GetHashCode(FootballTeam obj)
+        {
+            return obj.GetHashCode();
+        }
+
+    }
+    public interface IParsable
+    {
+        List<IParsable> Parse(string text);
+        void SaveAsXML();
+    }
+
+    class Parser
+    {
+        public List<IParsable> Parse(IParsable[] delegates, string text)
+        {
+            List<IParsable> res = new List<IParsable>();
+            foreach (IParsable p in delegates)
+            {
+                List<IParsable> temp = p.Parse(text);
+                res.Concat(temp);
+
+            }
+
+            return res;
+
+
+        }
+
+    }
+}
